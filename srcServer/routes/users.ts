4c5bf551b6510722011263d4752347352b7e3635
Julia Hagstedt <juliahagstedt@hotmail.com>
2025-10-23 20:36:13 +0200

    register start

---
+++ srcServer/routes/users.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+
+
+    
+})
+    
+export default router;
--- srcServer/server.ts
+++ srcServer/server.ts
@@ -1,9 +1,17 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 10000;
 
+app.use(cors());
+
 app.use(express.static("./dist/"));
+
+app.get('/', (req, res) => res.send('Chappy API online!'));
 
 
 app.listen(port, () => {
