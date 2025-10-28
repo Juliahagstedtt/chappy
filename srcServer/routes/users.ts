ee39ee3669a9a43274fd96c2fded55fc5334102a
Julia Hagstedt <juliahagstedt@hotmail.com>
2025-10-28 20:19:30 +0100

    JWT token klar och även user register

--- srcServer/routes/users.ts
+++ srcServer/routes/users.ts
@@ -1,78 +1,74 @@
-import express, { type Request, type Response, type Router } from 'express'
+import express, {} from 'express';
 import dotenv from 'dotenv';
 import { genSalt, hash } from "bcrypt";
 import crypto from "crypto";
-import db, { myTable } from '../data/dynamoDb.js'
-import { userPostSchema } from '../data/validation.js'
+import db, { myTable } from '../data/dynamoDb.js';
+import { userPostSchema } from '../data/validation.js';
 import { PutCommand } from '@aws-sdk/lib-dynamodb';
 import { createToken } from '../data/auth.js';
-
-
 dotenv.config();
 
 const router = express.Router();
 
 // Endpoint POST - registrera ny användare, hasha ett lösenord som sparas
+// Users med GET - Hämta info om inloggad användare
 
-// Users me GET - Hämta info om inloggad användare
-
-router.post('/register', async (req: Request, res: Response) => {
-
+router.post('/register', async (req, res) => {
+    
     const parsed = userPostSchema.safeParse(req.body);
+
     if (!parsed.success) {
-        return res.status(400).send({ error: "Ogitligt information" })
+        return res.status(400).send({ error: "Ogitligt information" });
     }
 
     const { username, password } = parsed.data;
-
-    // TODO: Skapa unikt ID
-
-    const userId = crypto.randomUUID();
-    console.log('Nytt användar Id', userId)
     
+    // TODO: Skapa unikt ID
+    const userId = crypto.randomUUID();
+    // console.log('Nytt användar Id', userId);
+
     // TODO: Hasha lösenordet med bcrypt
     const salt = await genSalt();
     const hashedPassword = await hash(password, salt);
-    console.log('Hashat lösenord', hashedPassword)
-
+    // console.log('Hashat lösenord', hashedPassword);
 
     // TODO: Fixa objektet som ska sparas i DynamoDB
     const userItem = {
-        PK: `USER#{userId}`,
-        SK: `INFO`,
+        Pk: `USER#${userId}`,
+        Sk: `INFO`,
         name: username,
         email: username,
         password: hashedPassword,
         type: 'user',
     };
-
-    console.log('User objekt redo att sparas', userItem)
-
-
+    // console.log('User objekt redo att sparas', userItem);
 
     // TODO: Spara användaren i DynamoDB
     try {
-        const command = new PutCommand ({
+        const command = new PutCommand({
             TableName: myTable,
-            Item: userItem 
+            Item: userItem
         });
 
         await db.send(command);
-        console.log('Användare sparad i DynomoDB');
+        // console.log('Användare sparad i DynomoDB');
 
-        } catch (error) {
-            console.log('Fel vid sparning', error);
-            return res.status(500).send({ error: 'Kunde inte spara användrae i DynamoDB' });
-        }
+        // TODO skapa JWT token senare
+        const token = createToken(userId);
 
-    // TODO: Response till frontend 
+        // TODO: Response till frontend 
         res.status(201).send({
             success: true,
             message: "Användare skapad",
-            userId: userId
+            userId: userId,
+            token
         });
-    // TODO skapa JWT token senare
-})
-    
+
+    } catch (error) {
+        console.log('Fel vid sparning', error);
+        return res.status(500).send({ error: 'Kunde inte spara användrae i DynamoDB' });
+    }
+
+});
 
 export default router;
